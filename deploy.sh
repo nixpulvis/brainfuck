58b7ad68a7c0721674b8f03ca3da6c399beda261
Nathan Lilienthal <nathan@nixpulvis.com>
2015-12-06 19:36:26 -0500

    Travis yo.

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rev=$(git rev-parse --short HEAD)
+
+cd target/doc
+
+git init
+git config user.name "Nathan Lilienthal"
+git config user.email "nathan@nixpulvis.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/nixpulvis/cs4740"
+git fetch upstream && git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
