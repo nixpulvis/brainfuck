56eccb107d1547225b8c8adfdca2356ea7ebcd59
Nathan Lilienthal <nathan@nixpulvis.com>
2015-12-06 19:39:13 -0500

    ffff

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git init
 git config user.name "Nathan Lilienthal"
 git config user.email "nathan@nixpulvis.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/nixpulvis/cs4740"
+git remote add upstream "https://$GH_TOKEN@github.com/nixpulvis/brainfuck"
 git fetch upstream && git reset upstream/gh-pages
 
 touch .
