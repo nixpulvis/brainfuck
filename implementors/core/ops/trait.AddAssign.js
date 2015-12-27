36df71d0a948caae7677fe51fbe1bf3dd69f94ee
Nathan Lilienthal <nathan@nixpulvis.com>
2015-12-27 13:24:23 +0000

    rebuild pages at d4c2b28

--- implementors/core/ops/trait.AddAssign.js
+++ implementors/core/ops/trait.AddAssign.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['brainfuck'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.AddAssign.html' title='core::ops::AddAssign'>AddAssign</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.u8.html'>u8</a>&gt; for <a class='struct' href='brainfuck/struct.Tape.html' title='brainfuck::Tape'>Tape</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.array.html'>[<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.u8.html'>u8</a>; TAPE_LENGTH]</a>&gt;",];
+implementors['brainfuck'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.AddAssign.html' title='core::ops::AddAssign'>AddAssign</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.u8.html'>u8</a>&gt; for <a class='struct' href='brainfuck/struct.Tape.html' title='brainfuck::Tape'>Tape</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/collections/vec/struct.Vec.html' title='collections::vec::Vec'>Vec</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.u8.html'>u8</a>&gt;&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
